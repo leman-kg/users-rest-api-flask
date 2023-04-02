68b7c310f2d82b79b90324f0ffc17b12e4839117
Leman <leman.kg@gmail.com>
2023-04-01 18:31:21 -0700

    rerender after file changes

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    if (dev) {
+      // Increase the polling interval to detect changes more frequently
+      config.watchOptions = {
+        poll: 1000, // Check for changes every second
+        aggregateTimeout: 300, // Delay rebuild by 300ms
+      };
+
+      // Only output minimal logging in development mode
+      config.stats = 'minimal';
+    }
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
